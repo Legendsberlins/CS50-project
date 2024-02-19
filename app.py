450f22df6cc196dd4526c384563ce2f8c3b80015
Legendsberlins <122905699+Legendsberlins@users.noreply.github.com>
2024-02-19 18:01:26 +0100

    sessions change

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
